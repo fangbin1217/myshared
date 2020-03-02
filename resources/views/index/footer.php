14d02471bf88a249a7697ab4ed467cc623d78c49
fangbin <jsonp@sina.com>
2020-03-02 11:06:27 +0800

    a

--- resources/views/index/footer.php
+++ resources/views/index/footer.php
@@ -1,11 +1,11 @@
-<script type='text/javascript' src='<?php echo config('local')['website']; ?>/static/js/theme/slides.js?ver=24/09/2017'></script>
-<script type='text/javascript' src='<?php echo config('local')['website']; ?>/static/js/theme/jquery.qrcode.min.js?ver=24/09/2017'></script>
-<script type='text/javascript' src='<?php echo config('local')['website']; ?>/static/js/theme/sticky.js?ver=1.6.0'></script>
-<script type='text/javascript' src='<?php echo config('local')['website']; ?>/static/js/theme/jquery-ias.js?ver=2.2.1'></script>
-<script type='text/javascript' src='<?php echo config('local')['website']; ?>/static/js/theme/jquery.lazyload.js?ver=24/09/2017'></script>
-<script type='text/javascript' src='<?php echo config('local')['website']; ?>/static/js/theme/tipso.js?ver=1.0.1'></script>
-<script type='text/javascript' src='<?php echo config('local')['website']; ?>/static/js/theme/script.js?ver=24/09/2017'></script>
-<script type='text/javascript' src='<?php echo config('local')['website']; ?>/static/js/theme/flexisel.js?ver=24/09/2017'></script>
-<script type='text/javascript' src='<?php echo config('local')['website']; ?>/static/js/theme/superfish.js?ver=24/09/2017'></script>
+<script type='text/javascript' src='<?php echo config('local')['imgHost']; ?>js/slides.js'></script>
+<script type='text/javascript' src='<?php echo config('local')['imgHost']; ?>js/jquery.qrcode.min.js'></script>
+<script type='text/javascript' src='<?php echo config('local')['imgHost']; ?>js/sticky.js'></script>
+<script type='text/javascript' src='<?php echo config('local')['imgHost']; ?>js/jquery-ias.js'></script>
+<script type='text/javascript' src='<?php echo config('local')['imgHost']; ?>js/jquery.lazyload.js'></script>
+<script type='text/javascript' src='<?php echo config('local')['imgHost']; ?>js/tipso.js'></script>
+<script type='text/javascript' src='<?php echo config('local')['imgHost']; ?>js/script.js'></script>
+<script type='text/javascript' src='<?php echo config('local')['imgHost']; ?>js/flexisel.js'></script>
+<script type='text/javascript' src='<?php echo config('local')['imgHost']; ?>js/superfish.js'></script>
 </body>
 </html>
